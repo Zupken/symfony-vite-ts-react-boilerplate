069531bcca99d0928db3446c3bbe8b443db4fff9
Zupken <Zupken@spoko.pl>
2025-10-04 09:44:05 +0200

    WIP

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,13 @@
 import { defineConfig } from "vite";
 import symfonyPlugin from "vite-plugin-symfony";
-
-/* if you're using React */
-// import react from '@vitejs/plugin-react';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
     plugins: [
-        /* react(), // if you're using React */
+        react(),
         symfonyPlugin(),
     ],
+    root: 'assets',
     build: {
         rollupOptions: {
             input: {
